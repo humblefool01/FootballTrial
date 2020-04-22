042beebdcdf00d75fea1bb4166cfed82db363ac3
Humblefool_01 <kaushikvishwanath010@gmail.com>
2020-04-22 14:54:01 +0530

    Updates

---
+++ precache-manifest.54e5548f5c49d5d12e113d47cc7a4922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb88c6409d824eb4ce794607e4d78b09",
+    "url": "/FootballTrial/index.html"
+  },
+  {
+    "revision": "77df2e20e51fd0e5c48c",
+    "url": "/FootballTrial/static/css/main.9573db0d.chunk.css"
+  },
+  {
+    "revision": "ae57223128eac3c18fa0",
+    "url": "/FootballTrial/static/js/2.2ae81ca6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FootballTrial/static/js/2.2ae81ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77df2e20e51fd0e5c48c",
+    "url": "/FootballTrial/static/js/main.c28c3a1c.chunk.js"
+  },
+  {
+    "revision": "a014d2f0c9661c41b227",
+    "url": "/FootballTrial/static/js/runtime-main.d3e3db10.js"
+  }
+]);
